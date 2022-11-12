4a96b1456b282c23fcb566d76fba1b90b07163ea
root <root@os3-360-13185.vs.sakura.ne.jp>
2022-11-12 17:54:50 +0900

    first commit

--- A/app.js
+++ A/app.js
@@ -1,18 +1,33 @@
+let debug = 0;
 const dgram = require('dgram');
+const net = require('net');
 
 const PORT_A = 8000;
 const HOST_A = 'a';
 
-const PORT_B = 8001;
-const HOST_B = 'b';
-
 const socket = dgram.createSocket('udp4');
+let count = 1;
 
 socket.on('message', (buf, rinfo) => {
-    console.log(rinfo.address + ':' + rinfo.port +' - ' + buf);
-    socket.send(buf, 0, buf.length, PORT_B, HOST_B, (err, bytes) => {
-        if (err) throw err;
-    });
+  console.log(rinfo.address + ':' + rinfo.port +' - ' + buf);
+  let str = buf.toString('ascii', 0, rinfo.size);
+  let msg = JSON.parse(str);
+  let ip = msg.i;
+  let port = msg.p;
+  // TCP
+  let client = net.connect(port, ip, () => {
+    console.log('connected to server:'+count);
+    client.write(count + 'Hello World!');
+  });
+  client.on('data', data => {
+    console.log('client-> ' + data);
+    client.destroy();
+  });
+  client.on('close', () => {
+    count += 1;
+    console.log('client-> connection is closed');
+  });
+
 });
 
 socket.bind(PORT_A, HOST_A);
--- B/app.js
+++ B/app.js
@@ -1,31 +1,32 @@
+let debug = 0;
 const dgram = require('dgram');
+const net = require('net');
 
+// Address of container A
 const PORT_A = 8000;
 const HOST_A = 'a';
-
+// Addresss of container B
 const PORT_B = 8001;
 const HOST_B = 'b';
 
-const socket = dgram.createSocket('udp4');
-
-var count = 0;
-
-setInterval(() => {
-    count++;
-    let data = {'c': count }
-    let json_str = JSON.stringify(data);
-    let message = new Buffer.from(json_str);
-    socket.send(message, 0, message.length, PORT_A, HOST_A, (err, bytes) => {
-        if (err) throw err;
+// TCP
+net.createServer(sock => {
+    sock.on('data', data => {
+        console.log(data + ' from ' + sock.remoteAddress + ':' + sock.remotePort);
+        sock.write('server -> Repeating: ' + data);
     });
-}, 500);
+    sock.on('close', () => {
+        console.log('client closed connection');
+    });
+}).listen(PORT_B,HOST_B);
 
-
-socket.on('message', (buf, rinfo) => {
-    console.log(rinfo.address + ':' + rinfo.port +' - ' + buf);
-    let str = buf.toString('ascii', 0, rinfo.size);
-    let msg = JSON.parse(str);
-    console.log(msg);
-});
-
-socket.bind(PORT_B, HOST_B);
+count = 0;
+// UDP
+while(count < 5){
+  const socket = dgram.createSocket('udp4');
+  let data = {'i':HOST_B,'p':PORT_B};
+  let json_str = JSON.stringify(data);
+  let message = new Buffer.from(json_str);
+  socket.send(message, 0, message.length, PORT_A, HOST_A, (err, bytes) => {if (err) throw err;});
+  count++;
+}
